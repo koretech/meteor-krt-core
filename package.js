4b38c553fc52c8cf6541f426b0a7b310f757dd7a
Mike Kornelson <mike@koretech.ca>
2014-10-28 09:51:07 -0500

    Update for atmosphere deploy

--- package.js
+++ package.js
@@ -1,6 +1,8 @@
 Package.describe({
-	summary: "Koretech Core Package",
-	version: '0.1.0'
+	name: 'krt:core',
+	summary: 'Koretech Core Package',
+	version: '0.1.0',
+	git: 'https://github.com/koretech/meteor-krt-core.git'
 });
 
 Package.onUse(function(api){
